504ec3f3a330a3a15f288b1dccff94ce361e5594
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-03-20 02:22:39 +0900

    add data and create bars on horizontal axis

--- step-count/script.js
+++ step-count/script.js
@@ -1,13 +1,16 @@
-const data = [550, 120, 731, 600]
+const data = [
+  50, 120, 73, 10, 5, 22, 31, 40, 50, 9, 31, 6, 55, 20, 73, 60, 50, 42, 71, 60,
+  15, 50, 11, 41, 20
+]
 
 const todaySvg = document.querySelector('svg')
 
 data.forEach((d, i) => {
   const rectTag = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
 
-  rectTag.setAttribute('x', '0')
-  rectTag.setAttribute('y', i * 45)
-  rectTag.setAttribute('width', d)
-  rectTag.setAttribute('height', '40')
+  rectTag.setAttribute('x', i * 36)
+  rectTag.setAttribute('y', 112 - d)
+  rectTag.setAttribute('width', 24)
+  rectTag.setAttribute('height', d)
   todaySvg.append(rectTag)
 })
