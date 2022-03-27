23c90f4f847a34ff6d132fde56ea21ffdefab360
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-03-27 21:10:40 +0900

    add custom data

--- step-count/today.js
+++ step-count/today.js
@@ -1,8 +1,3 @@
-const todayData = [
-  50, 120, 73, 10, 5, 22, 31, 40, 50, 9, 31, 6, 55, 20, 73, 60, 50, 42, 71, 60,
-  15, 50, 11, 41, 20
-]
-
 const todaySvg = d3.select('svg')
 
 const rectTags = d3.selectAll('rect')
@@ -12,11 +7,7 @@ todaySvg
   .data(todayData)
   .enter()
   .append('rect')
-  .attr('width', (d, i) => {
-    return d
-  })
-  .attr('y', (d, i) => {
-    return 20 * i
-  })
-  .attr('x', 0)
-  .attr('height', 16)
+  .attr('width', 24)
+  .attr('y', (d, i) => 112 - d)
+  .attr('x', (d, i) => i * 36)
+  .attr('height', (d, i) => d)
