7fd53fb1e266dffa9dc1652f7576395ad60d5fc8
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-03-27 21:26:10 +0900

    normalizing bar chart

--- step-count/today.js
+++ step-count/today.js
@@ -1,6 +1,6 @@
 const todaySvg = d3.select('svg')
 
-const rectTags = d3.selectAll('rect')
+const barScale = d3.scaleLinear().domain([0, 10000]).range([1, 112])
 
 todaySvg
   .selectAll('rect')
@@ -8,6 +8,6 @@ todaySvg
   .enter()
   .append('rect')
   .attr('width', 24)
-  .attr('y', (d, i) => 112 - d)
+  .attr('y', (d, i) => 112 - barScale(d))
   .attr('x', (d, i) => i * 36)
-  .attr('height', (d, i) => d)
+  .attr('height', (d, i) => barScale(d))
