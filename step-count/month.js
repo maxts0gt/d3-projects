618d804ed0e7eaf490810c9c9bb96ec5eca954fc
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-03 12:34:22 +0900

    add transition

--- step-count/month.js
+++ step-count/month.js
@@ -1,16 +1,36 @@
 const monthSvg = d3.select("svg.month");
 
+const radiusScale = d3.scaleSqrt().domain([0, 30000]).range([0, 50]);
+
+const colorScale = d3.scaleSqrt().domain([0, 30000]).range(["blue", "red"]);
+
 monthSvg
   .selectAll("circle")
   .data(monthData)
   .enter()
   .append("circle")
   .attr("cx", (d, i) => {
-    return i * 25;
+    return (i % 7) * 130 + 60;
   })
   .attr("cy", (d, i) => {
-    return 40;
+    return Math.floor(i / 7) * 130 + 60;
   })
+  .attr("r", 0)
+  .transition()
+  .duration(1000)
   .attr("r", (d, i) => {
-    return 5;
+    return radiusScale(d);
   });
+
+// monthSvg
+//   .selectAll("text")
+//   .data(todayData)
+//   .enter()
+//   .append("text")
+//   .attr("cx", (d, i) => {
+//     return i + 340;
+//   })
+//   .attr("cy", (d, i) => {
+//     return Math.floor(i / 7) * 130 + 400;
+//   })
+//   .text((d, i) => i + 1);
