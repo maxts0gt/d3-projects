6d0ba39df5e380ce70e6628ae4bb237a17352084
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-03 13:52:12 +0900

    refactor month into groups

--- step-count/month.js
+++ step-count/month.js
@@ -2,20 +2,24 @@ const monthSvg = d3.select("svg.month");
 
 const radiusScale = d3.scaleSqrt().domain([0, 30000]).range([0, 50]);
 
-const colorScale = d3.scaleSqrt().domain([0, 30000]).range(["blue", "red"]);
+// const colorScale = d3.scaleSqrt().domain([0, 30000]).range(["blue", "red"]);
 
-monthSvg
-  .selectAll("circle")
+const monthGroups = monthSvg
+  .selectAll("g")
   .data(monthData)
   .enter()
+  .append("g")
+  .attr("transform", (d, i) => {
+    const x = (i % 7) * 130 + 60;
+    const y = Math.floor(i / 7) * 150 + 60;
+    return `translate(${x}, ${y})`;
+  });
+
+monthGroups
   .append("circle")
-  .attr("cx", (d, i) => {
-    return (i % 7) * 130 + 60;
-  })
-  .attr("cy", (d, i) => {
-    return Math.floor(i / 7) * 130 + 60;
-  })
-  .attr("r", (d, i) => i - i * 0.7)
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 10)
   .transition()
   .duration(250)
   .delay((d, i) => {
