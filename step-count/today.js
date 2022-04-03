e0b071e549d926e3bb5a1fb58d5c16253bd5ee2d
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-03 11:41:32 +0900

    add date text

--- step-count/today.js
+++ step-count/today.js
@@ -1,13 +1,26 @@
-const todaySvg = d3.select('svg')
+const todaySvg = d3.select("svg");
 
-const barScale = d3.scaleLinear().domain([0, 10000]).range([1, 112])
+const barScale = d3.scaleLinear().domain([0, 10000]).range([1, 112]);
 
 todaySvg
-  .selectAll('rect')
+  .selectAll("rect")
   .data(todayData)
   .enter()
-  .append('rect')
-  .attr('width', 24)
-  .attr('y', (d, i) => 112 - barScale(d))
-  .attr('x', (d, i) => i * 36)
-  .attr('height', (d, i) => barScale(d))
+  .append("rect")
+  .attr("width", 24)
+  .attr("y", (d, i) => 112 - barScale(d))
+  .attr("x", (d, i) => i * 36)
+  .attr("height", (d, i) => barScale(d));
+
+todaySvg
+  .selectAll("text")
+  .data(todayData)
+  .enter()
+  .append("text")
+  .attr("x", (d, i) => {
+    return i * 36 + 12;
+  })
+  .attr("y", 130)
+  .text((d, i) => {
+    return i + 1;
+  });
